33dc977ab4dd8742390f33d0ccd99638a3e51973
MaierEduard <maiereduard86@gmail.com>
2019-02-11 22:34:30 +0200

    Register Test

---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.security.Key;
+
+public class RegisterTest {
+
+
+
+    @Test
+    public void registerTest() {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test");
+
+
+        driver.findElement(By.linkText("ACCOUNT")).click();
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Maier");
+        driver.findElement(By.name("middlename")).sendKeys("Eduard");
+        driver.findElement(By.name("lastname")).sendKeys("Lotzi");
+        driver.findElement(By.id("email_address")).sendKeys("maiereduard86@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.name("confirmation")).sendKeys("password");
+        driver.findElement(By.className("checkbox")).click();
+
+    }
+}
